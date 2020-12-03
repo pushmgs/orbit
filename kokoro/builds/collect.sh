f5421cef80f750c7febbe84aff9e7fb3b45df0f3
Daniel Fenner <dfenner@google.com>
2020-12-03 18:24:13 +0100

    Implement collection of artefacts.

--- kokoro/builds/collect.sh
+++ kokoro/builds/collect.sh
@@ -1,4 +1,9 @@
 # Fail on any error.
 set -e
 
-sleep 10000;
+# Trivial repackaging of the build artefacts: The collector gets integrated into Orbit.zip.
+cd "$KOKORO_GFILE_DIR"
+unzip Collector.zip
+unzip Orbit.zip
+rm Orbit.zip
+zip -r Orbit.zip Orbit/
